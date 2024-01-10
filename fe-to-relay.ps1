cf0e62a020e0293a0c1982c46d85caa86c1a978d
Christian Kollross <christian@kollross.name>
2024-01-10 07:27:30 +0100

    Improved PowerShell script to handle multiple COM ports

--- fe-to-relay.ps1
+++ fe-to-relay.ps1
@@ -1,13 +1,26 @@
 
 # Seriellen Port ermitteln - funktioniert nicht, wenn mehrere vorhanden sind
 $COM = [System.IO.Ports.SerialPort]::getportnames()
+Write-Host "Gefundene Ports: $COM"
 
-# Serielle Verbindung öffnen
-$port= new-Object System.IO.Ports.SerialPort $COM,9600,None,8,one
-$port.open()
-# Das Öffnen des Ports resettet den Arduino, darum warten wir vor dem Senden kurz
-sleep 3
+$portList = $COM.Split(" ")
 
-# Sende "x", da der Arduino nur auf dieses Zeichen hört, um Fehlauslösungen zu vermeiden
-$port.WriteLine("x")
-$port.Close()
+foreach($portName in $portList) {
+	Write-Host "Versuche Port $portName"
+
+	# Serielle Verbindung öffnen
+	$port= new-Object System.IO.Ports.SerialPort $portName,9600,None,8,one
+	$port.ReadTimeout = 200
+	$port.WriteTimeout = 200
+	
+	$port.open()
+	# Das Öffnen des Ports resettet den Arduino, darum warten wir vor dem Senden kurz
+	Start-Sleep -Seconds 1
+
+	# Sende "x", da der Arduino nur auf dieses Zeichen hört, um Fehlauslösungen zu vermeiden
+	$port.WriteLine("x")
+	$port.Close()
+}
+
+Write-Host "Programm wird nach 30 Sekunden geschlossen..."
+Start-Sleep -Seconds 30
