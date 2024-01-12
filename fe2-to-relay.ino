5311f00a3e8151c644d14888985ca730d3d759bd
Christian Kollross <christian@kollross.name>
2024-01-12 19:22:41 +0100

    Cleaned up Arduino code comments

--- fe2-to-relay.ino
+++ fe2-to-relay.ino
@@ -2,8 +2,8 @@
 // FE2 to relay
 // ============
 
-#define OUTPUT_PIN 2        // Pin which will be set to 5V (high) after the serial command
-#define LED_PIN 11 // Set to LED_BUILTIN for onboard LED
+#define OUTPUT_PIN 2        // Pin which will be set to 5V (high) after receiving serial command
+#define LED_PIN 11          // Set to "LED_BUILTIN" instead of "11" to use onboard LED
 #define ON_DURATION_MS 3000 // Duration in milliseconds, for which OUTPUT_PIN will be set to high
 
 int incomingByte = 0;
